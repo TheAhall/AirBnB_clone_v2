b5fd43d863c83f825c0e60f3573aced29f3c8fa0
HALLAL <hmdhallal@gmail.com>
2024-04-16 15:58:26 -0700

    0x02. AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -54,6 +54,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         """Deletes obj from __objects if it's inside"""
         if obj is not None:
