6b526b644dce76bff08bd949eaf747823eb4dcc9
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-04-17 16:52:04 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,9 +7,12 @@ from sqlalchemy import Column, String
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User"""
+        super().__init__(*args, **kwargs)
