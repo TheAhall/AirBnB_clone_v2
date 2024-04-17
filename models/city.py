bdef389810248df3fb356d79d38caad2790ee0a7
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-04-17 17:20:45 +0200

    0x02. AirBnB clone - MySQL

--- models/city.py
+++ models/city.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
