bd448531d68162fa40beeda2a2959d4c3f452d6b
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-04-17 17:34:52 +0200

    0x02. AirBnB clone - MySQL

--- models/place.py
+++ models/place.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
