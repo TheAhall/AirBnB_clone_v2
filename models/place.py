da0628800ce7383a52b90214e2009f9da59540c5
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-04-18 02:46:08 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -19,3 +19,30 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                              Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+
+    else:
+        @property
+        def amenities(self):
+            """Getter attribute that returns the list of Amenity instances
+            based on the attribute amenity_ids that contains all Amenity.id
+            linked to the Place"""
+            from models import storage
+            amenity_objs = []
+            for amenity_id in self.amenity_ids:
+                key = "Amenity." + amenity_id
+                amenity_objs.append(storage.all()["Amenity"][key])
+            return amenity_objs
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute that handles append method for adding
+            an Amenity.id to the attribute amenity_ids"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
