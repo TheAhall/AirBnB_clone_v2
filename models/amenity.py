9ee0c81260ec791af2977db12d61d0130e52a03e
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-04-18 03:12:45 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """ Amenity class to store amenity information """
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
-
-    place_amenities = Table('place_amenities', Base.metadata,
-                            Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
-
-    places = relationship("Place", secondary=place_amenities, back_populates="amenities")
+    place_amenities = relationship("Place", secondary='place_amenity', back_populates='amenities')
