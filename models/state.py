2578c10a267bc85011b78af519bf02bc97e5cc70
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-05-18 00:30:47 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
+#!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
